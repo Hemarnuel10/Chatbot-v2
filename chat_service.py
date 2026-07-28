56502990e45513724d3661824eea70a7cc8d1d63
Abegunde Emmanuel <emmanuelabegunde2007@gmail.com>
2026-07-28 10:36:48 +0100

    Fixed  Gemini.py, providerManager and chatService

---
+++ chat_service.py
@@ -0,0 +1,31 @@
+from Services.history_service import save_chat, get_recent_history
+from Services.prompt_builder import build_prompt
+from provider.provider_manager import ProviderManager
+class ChatService:
+
+    def chat(self, user_message):
+        
+        # Load previous conversation
+        conversation = get_recent_history(10)
+
+        # Build prompt
+        full_prompt = build_prompt(
+            conversation,
+            user_message
+        )
+
+        # Get current AI provider
+        provider = ProviderManager.get_provider()
+        
+        # Generate response
+        ai_response = provider.generate(full_prompt)
+
+        # Save chat
+        save_chat(
+            user_message,
+            ai_response,
+            provider.model
+        )
+
+        # Return response
+        return ai_response
